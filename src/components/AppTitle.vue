523fefaeb94f8c81fb234215181fbb6e22334e59
davidecaputo <daca9621@gmail.com>
2023-04-12 18:33:07 +0200

    Add script

--- src/components/AppTitle.vue
+++ src/components/AppTitle.vue
@@ -1,3 +1,14 @@
 <template>
     <h1>La mia prima app con Vite!</h1>
 </template>
+
+<script>
+export default {
+    name: "AppTitle",
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
